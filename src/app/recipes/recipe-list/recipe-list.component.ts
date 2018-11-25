c9fe7cf93d99cc23f1a99bd1aedf9b00a9f0a09c
alvin <alvin.leung92@gmail.com>
2018-11-24 21:37:49 -0800

    Layout recipe components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      "Mighty Burger", 
+      "The mightiest burger of them all", 
+      "https://www.tasteofhome.com/wp-content/uploads/2017/10/exps28800_UG143377D12_18_1b_RMS-696x696.jpg"),
+    new Recipe(
+      "Spaghetti Sauce with Ground Beef", 
+      "Back to basics", 
+      "https://tmbidigitalassetsazure.blob.core.windows.net/secure/RMS/attachments/37/1200x1200/exps108178_TH163620B11_11_3b.jpg")
+  ];
 
   constructor() { }
 
