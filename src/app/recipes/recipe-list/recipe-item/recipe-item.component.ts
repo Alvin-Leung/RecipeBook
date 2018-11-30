9f51829271526e4b9ef7af3214e60b48ddf719d4
alvin <alvin.leung92@gmail.com>
2018-11-29 21:41:45 -0800

    Populate recipe detail component when recipe item is clicked

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../../recipe.model';
 
 @Component({
@@ -13,5 +13,4 @@ export class RecipeItemComponent implements OnInit {
 
   ngOnInit() {
   }
-
 }
